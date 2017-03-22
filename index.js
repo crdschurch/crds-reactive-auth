4ea68fe0697887d7f162523c10684003fd2d808c
Jon Horner <hornerjn@mail.uc.edu>
2017-03-22 16:26:29 -0400

    US7410 Fleshed out the main class
    Haven't written unit tests yet so everything is still subject to change

--- index.js
+++ index.js
@@ -79,23 +79,131 @@ var _createClass = (function () { function defineProperties(target, props) { for
 var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
 
 var ReactiveAuth = (function () {
-  function ReactiveAuth() {
+
+  /**
+   * Handles watching for changes with the auth cookie and dispatches the
+   *     appropriate events.
+   *
+   * @constructor
+   * @param {string} name - the name of the auth cookie that should be watched.
+   * @param {Function} defaultHandler - A default callback for the event
+   *     listeners. @default console.log
+   */
+
+  function ReactiveAuth(name, defaultHandler) {
     _classCallCheck(this, ReactiveAuth);
+
+    // If name is falsy then set the cookieName to an empty string
+    var cookieName = "" + (name || "sessionId");
+
+    // This regex extracts the value of the cookie `cookieName`
+    this.cookieValRe = new RegExp("(?:(?:^|.*;\\s*)" + cookieName + "\\s*\\=\\s*([^;]*).*$)|^.*$", "");
+    this.cookieVal = undefined;
+    this.watchAuthCookie = undefined;
+
+    /* eslint-disable no-console */
+    // Sets up the default event handler for the update and expire events
+    this.updateHandler = defaultHandler && typeof defaultHandler === "function" ? defaultHandler : console.log;
+    this.expireHandler = defaultHandler && typeof defaultHandler === "function" ? defaultHandler : console.log;
+    /* eslint-enable no-console */
   }
 
   _createClass(ReactiveAuth, {
     subscribe: {
-      value: function subscribe() {
-        // Public API for subscribing to the auth events
 
-        return this;
+      /**
+       * Subscribes the client to events that are dispatched by this class.
+       *
+       * @param {number} freq - the frequency in which changes should be looked for.
+       *     @default 3000 (milliseconds)
+       * @param {Function} updateCallback - a specific function that should be called
+       *     when the `updateAuth` event is dispatched.
+       * @param {Function} expireCallback - a specific function that should be called
+       *     when the `expireAuth` event is dispatched.
+       * @returns {number} The ID of the interval that was set.
+       */
+
+      value: function subscribe(freq, updateCallback, expireCallback) {
+        var _this = this;
+
+        this.createEventListeners(updateCallback, expireCallback);
+
+        if (this.watchAuthCookie) {
+          clearInterval(this.watchAuthCookie);
+        }
+
+        var frequency = freq > 0 ? freq : 3000;
+
+        this.watchAuthCookie = setInterval(function () {
+          var browserCookieVal = document.cookie.replace(_this.cookieValRe, "$1");
+
+          if (!browserCookieVal && _this.cookieVal) {
+            // The cookie expired
+            window.dispatchEvent(new CustomEvent("expireAuth", {
+              detail: {
+                message: "Auth Cookie Expired",
+                oldValue: _this.cookieVal,
+                currentValue: browserCookieVal },
+              bubbles: true,
+              cancelable: true
+            }));
+
+            _this.cookieVal = browserCookieVal;
+          } else if (_this.cookieVal !== browserCookieVal) {
+            // The cookie was updated manually by the user
+            window.dispatchEvent(new CustomEvent("updateAuth", {
+              detail: {
+                message: "Updating Auth Cookie",
+                oldValue: _this.cookieVal,
+                currentValue: browserCookieVal },
+              bubbles: true,
+              cancelable: true
+            }));
+
+            _this.cookieVal = browserCookieVal;
+          }
+        }, frequency);
+
+        return this.watchAuthCookie;
       }
     },
     unsubscribe: {
-      value: function unsubscribe() {
-        // Public API for unsubscribing from the auth events
 
-        return this;
+      /**
+       * Unsubscribes the client from the events that this class dispatches
+       *     and stops the interval that was running.
+       */
+
+      value: function unsubscribe() {
+        clearInterval(this.watchAuthCookie);
+        this.watchAuthCookie = undefined;
+
+        window.removeEventListener("updateAuth");
+        window.removeEventListener("expireAuth");
+      }
+    },
+    createEventListeners: {
+
+      /**
+       * Creates the event listeners for the `updateAuth` and `expireAuth` events
+       *     and attaches them to the DOM.
+       * @private
+       * @param {Function} updateCb - a specific function that should be called
+       *     when the `updateAuth` event is dispatched.
+       * @param {Function} expireCb - a specific function that should be called
+       *     when the `expireAuth` event is dispatched.
+       */
+
+      value: function createEventListeners(updateCb, expireCb) {
+        if (updateCb && typeof updateCb === "function") {
+          this.updateHandler = updateCb;
+        }
+        if (expireCb && typeof expireCb === "function") {
+          this.expireHandler = expireCb;
+        }
+
+        window.addEventListener("updateAuth", this.updateHandler, false);
+        window.addEventListener("expireAuth", this.expireHandler, false);
       }
     }
   });
